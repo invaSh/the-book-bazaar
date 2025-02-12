b7f19e2afc16fb121df845df95b4cc547a5a6734
invaSh <inva_sh@hotmail.com>
2025-02-12 19:45:23 +0100

    Identity Service: Creation, client configuration

---
+++ src/IdentityService/Config.cs
@@ -0,0 +1,37 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityService;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope("book_bazaar"),
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+            {
+                ClientId = "book_bazaar_client",
+                ClientName = "book_bazaar_client",
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = { "https://localhost:5173/signin-oidc" },
+                PostLogoutRedirectUris = { "https://localhost:5173/" },
+                AllowedCorsOrigins = { "https://localhost:5173" },
+                AllowedScopes = { "openid", "profile", "email", "book_bazaar" },
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                RequirePkce = true,
+            }
+        };
+}
